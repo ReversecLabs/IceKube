62ca6c6be36fb5c51ef8c7a5c31009fcd9435e7d
Mohit Gupta <mohit.gupta@reversec.com>
2026-02-10 16:46:48 +0000

    doesn't have service accounts make the relationship between secrets unless the secret doesn't exist in the raw data

--- icekube/models/serviceaccount.py
+++ icekube/models/serviceaccount.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 from functools import cached_property
-from typing import List
+from typing import List, cast
 
 from icekube.models.base import RELATIONSHIP, Resource
 from icekube.models.secret import Secret
@@ -30,7 +30,25 @@ class ServiceAccount(Resource):
         initial: bool = True,
     ) -> List[RELATIONSHIP]:
         relationships = super().relationships()
-        relationships += [
-            (x, Relationship.AUTHENTICATION_TOKEN_FOR, self) for x in self.secrets
-        ]
+
+        # This is to account for secrets that we have not been able to get the metadata for
+        # If we have the metadata, the relevant relationship will be generated from that
+        # resource
+        SECRET_QUERY = "MATCH ({prefix}:Secret {{ namespace: ${prefix}_namespace, name: ${prefix}_name }})"
+        SECRET_QUERY += " WHERE {prefix}.raw IS NULL "
+
+        if initial:
+            relationships += [(self, Relationship.REFERENCES, x) for x in self.secrets]
+        else:
+            relationships += [
+                (
+                    (
+                        SECRET_QUERY,
+                        {"namespace": cast(str, x.namespace), "name": x.name},
+                    ),
+                    Relationship.AUTHENTICATION_TOKEN_FOR,
+                    self,
+                )
+                for x in self.secrets
+            ]
         return relationships
